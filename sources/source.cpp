bf1372030625f1a76ad75f0ea2d9a29e7d010c05
VToreZ <lumisitone@gmail.com>
2020-02-10 14:01:44 +0300

    init

---
+++ sources/source.cpp
@@ -0,0 +1,20 @@
+//Copyright 2019 <VToreZ>
+
+#include <stack1.hpp>
+#include <stack2.hpp>
+int main(){
+    stack1<int> t;
+    stack2<int> y;
+    for (int i = 0; i < 10; i++)
+        t.push(i);
+    for (int i = 0; i < 10; i++){
+        std::cout << t.head() << "\t";
+        t.pop();
+    }
+    y.push_emplace(0, 1, 2);
+    for (int i = 0; i < 3; i++){
+        std::cout << y.head() << "\t";
+        y.pop();
+    }
+    return 0;
+}
